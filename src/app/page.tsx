d17e1566ecba3b7785da782055fcf116d3445cb9
Jesray Garciano <jesraygarciano@Jesrays-MacBook-Pro.local>
2024-06-21 20:51:50 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,234 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useRef, useState } from "react";
+import * as faceapi from "face-api.js";
+import Webcam from "react-webcam";
+import styles from "./styles.module.css";
+
+const Home = () => {
+  const videoRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  const [blinkCount, setBlinkCount] = useState(0);
+  const [nowBlinking, setNowBlinking] = useState(false);
+  const [irisC, setIrisC] = useState<number[]>([]);
+  // const mBlinkSound = new Audio("/sound/shotgun-firing1.mp3");
+
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = "/facemodels";
+      await Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]);
+      setModelsLoaded(true);
+    };
+    loadModels();
+  }, []);
+
+  useEffect(() => {
+    const startVideo = async () => {
+      const video = videoRef.current?.video as HTMLVideoElement;
+      if (!video) return;
+
+      if (navigator.userAgent.match(/iPhone|iPad|Android/)) {
+        console.log("Mobile");
+        video.width = 400;
+
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false,
+          });
+          video.srcObject = stream;
+          video.play();
+        } catch (err) {
+          console.error("Not available!!!!", err);
+        }
+      } else {
+        console.log("PC");
+        navigator.getUserMedia(
+          { video: {} },
+          (stream) => {
+            video.srcObject = stream;
+            video.play();
+          },
+          (err) => console.error(err)
+        );
+      }
+    };
+
+    if (modelsLoaded) {
+      startVideo();
+    }
+  }, [modelsLoaded]);
+
+  useEffect(() => {
+    const handleVideoPlay = () => {
+      const video = videoRef.current?.video as HTMLVideoElement;
+      const canvas = canvasRef.current;
+      if (!video || !canvas) return;
+
+      const displaySize = {
+        width: video.videoWidth,
+        height: video.videoHeight,
+      };
+      faceapi.matchDimensions(canvas, displaySize);
+
+      const canvas_bg = document.createElement("canvas");
+      canvas_bg.width = video.videoWidth;
+      canvas_bg.height = video.videoHeight;
+      document.body.append(canvas_bg);
+      const ctx_bg = canvas_bg.getContext("2d");
+
+      const canvas_face = document.createElement("canvas");
+      canvas_face.width = video.videoWidth;
+      canvas_face.height = video.videoHeight;
+      const ctx_face = canvas_face.getContext("2d");
+
+      let t1 = performance.now();
+
+      const interval = setInterval(async () => {
+        const detections = await faceapi
+          .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+          .withFaceLandmarks();
+        const resizedDetections = faceapi.resizeResults(
+          detections,
+          displaySize
+        );
+        const ctx = canvas.getContext("2d");
+        ctx?.clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+
+        if (resizedDetections.length > 0) {
+          const landmarks = resizedDetections[0].landmarks.positions;
+
+          ctx_bg?.clearRect(0, 0, canvas_bg.width, canvas_bg.height);
+          let x_ = landmarks[37].x;
+          let y_ = landmarks[37].y;
+          let w_ = landmarks[38].x - landmarks[37].x;
+          let h_ = landmarks[41].y - landmarks[37].y;
+          ctx_bg?.fillRect(x_, y_, w_, h_);
+
+          x_ = landmarks[43].x;
+          y_ = landmarks[43].y;
+          w_ = landmarks[44].x - landmarks[43].x;
+          h_ = landmarks[47].y - landmarks[43].y;
+          ctx_bg?.fillRect(x_, y_, w_, h_);
+
+          ctx_face?.clearRect(0, 0, canvas_face.width, canvas_face.height);
+          ctx_face?.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+          const frame = ctx_face?.getImageData(
+            0,
+            0,
+            video.videoWidth,
+            video.videoHeight
+          );
+          const p_ =
+            Math.floor(x_ + w_ / 2) +
+            Math.floor(y_ + h_ / 2) * video.videoWidth;
+          const v_ = Math.floor(
+            (frame?.data[p_ * 4 + 0] +
+              frame?.data[p_ * 4 + 1] +
+              frame?.data[p_ * 4 + 2]) /
+              3
+          );
+
+          setIrisC((prev) => {
+            const newIrisC = [...prev, v_];
+            if (newIrisC.length > 100) newIrisC.shift();
+            return newIrisC;
+          });
+
+          const meanIrisC =
+            irisC.reduce((sum, val) => sum + val, 0) / irisC.length;
+          const vThreshold = 1.5;
+          const currentIrisC = irisC[irisC.length - 1];
+
+          if (irisC.length === 100) {
+            if (!nowBlinking && currentIrisC >= meanIrisC * vThreshold) {
+              setNowBlinking(true);
+            } else if (nowBlinking && currentIrisC < meanIrisC * vThreshold) {
+              setNowBlinking(false);
+              setBlinkCount((prev) => prev + 1);
+              // mBlinkSound.pause();
+              // mBlinkSound.currentTime = 0;
+              // mBlinkSound.play();
+            }
+          }
+
+          ctx_bg.strokeStyle = "red";
+          ctx_bg.lineWidth = 5;
+          const Ox = 0;
+          const Oy = canvas_bg.height / 2;
+          const Lx = canvas_bg.width;
+          const Ly = canvas_bg.height / 2;
+          let vx = (0 / irisC.length) * Lx;
+          let vy = (irisC[0] / 255) * Ly;
+          ctx_bg.beginPath();
+          ctx_bg.moveTo(Ox + vx, Oy - vy);
+          for (let i = 1; i < irisC.length; i++) {
+            vx = (i / irisC.length) * Lx;
+            vy = (irisC[i] / 255) * Ly;
+            ctx_bg.lineTo(Ox + vx, Oy - vy);
+          }
+          ctx_bg.stroke();
+
+          ctx_bg.strokeStyle = "rgb(0,255,0)";
+          ctx_bg.lineWidth = 2;
+          ctx_bg.beginPath();
+          vx = 0 * Lx;
+          vy = ((meanIrisC * vThreshold) / 255) * Ly;
+          ctx_bg.moveTo(Ox + vx, Oy - vy);
+          vx = 1 * Lx;
+          ctx_bg.lineTo(Ox + vx, Oy - vy);
+          ctx_bg.stroke();
+
+          const t2 = performance.now();
+          ctx.font = "48px serif";
+          ctx.fillText("FPS:" + Math.floor(1000.0 / (t2 - t1)), 10, 50);
+          ctx.fillText("Count:" + blinkCount, 10, 100);
+          if (nowBlinking) {
+            ctx.fillText("Blinking", 10, 150);
+          }
+          t1 = t2;
+        }
+      }, 33);
+
+      return () => clearInterval(interval);
+    };
+
+    if (videoRef.current) {
+      const video = videoRef.current.video as HTMLVideoElement;
+      video.addEventListener("play", handleVideoPlay);
+      return () => {
+        video.removeEventListener("play", handleVideoPlay);
+      };
+    }
+  }, [modelsLoaded, irisC, nowBlinking]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className={styles.container}>
+      <Webcam
+        ref={videoRef}
+        className={styles.video}
+        onLoadedMetadata={() => {
+          const video = videoRef.current?.video as HTMLVideoElement;
+          if (video) {
+            video.play();
+          }
+        }}
+      />
+      <canvas ref={canvasRef} className={styles.canvas} />
+      <div className={styles.info}>
+        <p>FPS: {Math.floor(1000.0 / 33)}</p>
+        <p>Blink Count: {blinkCount}</p>
+        {nowBlinking && <p>Blinking</p>}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Home;
